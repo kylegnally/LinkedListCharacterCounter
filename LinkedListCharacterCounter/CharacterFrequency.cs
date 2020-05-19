4055f3669984823a4feab443d5aef6f1f7ed629e
occulus1975 <occulus1975@gmail.com>
2020-05-19 14:19:46 -0400

    Begin addin code for CharacterFrequency.

---
+++ LinkedListCharacterCounter/CharacterFrequency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListCharacterCounter
+{
+    class CharacterFrequency
+    {
+        private char _char;
+        private int _freq;
+
+        public char Character { get; set; }
+        public long Frequency { get; set; }
+
+        public CharacterFrequency(char aChar)
+        {
+        }
+
+        public void IncrementFrequency()
+        {
+            _freq++;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);    
+        }
+
+        public override string ToString()
+        {
+            string output = String.Format(_char + " " + "(" + (byte) _char + ")" + "\t" + " = " + _freq);
+            return output;
+        }
+    }
+}
