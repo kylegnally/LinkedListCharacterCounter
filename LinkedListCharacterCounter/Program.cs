cb50ccf00e2d194a010ac9dac657a0d53584c1b7
occulus1975 <occulus1975@gmail.com>
2020-05-19 13:57:54 -0400

    Add project files.

---
+++ LinkedListCharacterCounter/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListCharacterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            args = new[] { "testfile.txt" };
+#endif
+
+            if (args.Length > 1)
+            {
+                Console.WriteLine("File writing is unsupported.");
+                Environment.Exit(0);
+            }
+            else HandleFile(ReadFile(args));
+
+        }
+
+        private static char[] ReadFile(string[] args)
+        {
+            char[] chars = null;
+            if (args != null)
+            {
+                if (File.Exists(args[0]))
+                {
+                    try
+                    {
+                        string fullPath = Environment.CurrentDirectory + "\\" + args[0];
+                        using (StreamReader reader = new StreamReader(fullPath))
+                        {
+                            char[] contents = reader.ReadToEnd().ToCharArray();
+                            return contents;
+                        }
+                    }
+                    catch
+                    {
+                        throw new FileLoadException();
+                    }
+                }
+                throw new FileNotFoundException();
+            }
+            throw new NullReferenceException();
+        }
+
+        private static void HandleFile(char[] chars)
+        {
+
+
+        }
+    }
+}
